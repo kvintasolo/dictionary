23911a931dab2b3a0eda5b0465cb3116ebb1880a
Anna Stratanovych <105247537+kvintasolo@users.noreply.github.com>
2023-02-14 17:38:31 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Dictionary from './Dictionary';
 export default function App() {
   return (
     <div className="App">
-        
+        <div className='container'>
        <Dictionary />
       <footer>
         This project was coded by <a href="https://serene-mandazi-45beb2.netlify.app/" target="_blank" rel="noreferrer"> Anna Stratanovych</a> and is  
         <a href="https://github.com/kvintasolo/dictionary" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
       </footer>
+      </div>
     </div>
   );
 }
